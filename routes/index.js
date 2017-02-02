b653d923bcd59be6f3364fe8501e3290b2533e68
Ben Gott <bengott@gmail.com>
2017-02-01 21:22:14 -0600

    add comicID routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var title = 'Marvelous Squishy Connection Machine';
+var latestComicID = 'sleep-loves-me'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Marvelous Squishy Connection Machine' });
+  res.render('index', {
+    title: title,
+    comicID: latestComicID,
+    comicTitle: latestComicID.replace(/\-/g, ' '), 
+    imagePath: '/images/' + latestComicID + '.jpg'
+  });
+});
+
+/* GET a comic page. */
+router.get('/:comicID', function(req, res, next) {
+  res.render('index', {
+    title: title,
+    comicID: req.params.comicID,
+    comicTitle: req.params.comicID.replace(/\-/g, ' '), 
+    imagePath: '/images/' + req.params.comicID + '.jpg'
+  });
 });
 
 module.exports = router;
